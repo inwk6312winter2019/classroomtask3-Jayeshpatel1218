ca572dbd0e61a05dcd5597f80649ab2c558cc027
Jayeshpatel1218 <jy331399@dal.ca>
2019-02-04 15:09:21 +0000

    solution for classtask3

--- clasroomtask3.py
+++ clasroomtask3.py
@@ -1,9 +1,11 @@
-rank = [1,2,3,4,5,6,7,8,9,10,11,12,13]
-suite = ["H","S","D","C"]
-
-cards = list(zip(rank,suite))
-cards = []
-for ranks in rank:
-    cards = cards + [(ranks,suite[0]),(ranks,suite[1]),ranks,suite[2]]
-
-print(cards)
+class card():
+  """this creates a card object"""
+  suit_names = ['Clubs', 'Diamonds', 'Hearts', 'Spades'] 
+  rank_names = [None, 'Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+  def __init__(self,rank=0,suite=2):
+    self.rank = rank
+    self.suite =  suite
+  def __str__(self):
+    return f"the rank is {card.rank_names[self.rank]} and the suit is {card.suit_names[self.suite]}"
+ace_of_spade = card(1,3)
+print(ace_of_spade)
